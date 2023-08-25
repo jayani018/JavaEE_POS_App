7ed77fe1bff80ea21c07703b4619419b69e64ec9
jayani018 <jayaniarunika11@gmail.com>
2023-08-25 08:28:00 +0530

    Updated index file and html pages

--- Back_End/src/lk/ijse/pos/servlet/CustomerServletAPI.java
+++ Back_End/src/lk/ijse/pos/servlet/CustomerServletAPI.java
@@ -1,9 +1,47 @@
 package lk.ijse.pos.servlet;
 
+import com.mysql.jdbc.Driver;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
 /**
  * @author : Jayani_Arunika  8/24/2023 - 2:27 PM
  * @since : v0.01.0
  **/
 
-public class CustomerServletAPI {
+@WebServlet(urlPatterns = "/pages/spa")
+public class CustomerServletAPI extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos_system", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("select * from Customer");
+            ResultSet rst = pstm.executeQuery();
+
+            JsonArrayBuilder allCustomers = Json.createArrayBuilder();
+            while (rst.next()) {
+                String id = rst.getString(1);
+                String name = rst.getString(2);
+                String address = rst.getString(3);
+
+                JsonObjectBuilder customerObject = Json.createObjectBuilder();
+                customerObject.add("id", id);
+                customerObject.add("name", name);
+                customerObject.add("address", address);
+                allCustomers.add(customerObject.build());
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+
+        }
+    }
 }
